2eee5dc10e37c172b06baedb4b49af7161736ab4
andyg7 <andrewgrant243@gmail.com>
2017-04-14 19:25:00 +0000

    Add path_exists code

--- new_src/path_algorithms.h
+++ new_src/path_algorithms.h
@@ -5,6 +5,18 @@
 #include "path_algorithms_structs.h"
 #include "node_expander_path_algorithms.h"
 
+
+template<typename G, typename N>
+bool path_exists(G graph_data, N start_node, N goal_node)
+{
+
+	using weight_type = typename G::element_type::weight_type;
+	using path_state_type = path_state<G, N, weight_type>;
+	shared_ptr<path_state_type> start_state = make_shared<path_state_type>(start_node, 0, nullptr, graph_data);
+	shared_ptr<path_state_type> goal_state = make_shared<path_state_type>(goal_node, 0, nullptr, graph_data);
+	return path_exists(start_state, goal_state);
+}
+
 template<typename G, typename N>
 void find_path_dfs(G graph_data, N start_node, N goal_node)
 {
